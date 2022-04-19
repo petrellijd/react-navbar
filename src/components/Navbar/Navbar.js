2954c7f81961f98b68cb46e6605bde29bfb68c47
petrellijd <petrellijd@gmail.com>
2022-04-19 15:04:10 -0600

    adding files

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {MenuItems} from './MenuItems';
+import './Navbar.css';
+import {Button} from '../Button';
+
+class Navbar extends React.Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className='navbar-logo'>Poundin' Keys<i className='fab fa-grav'></i></h1>
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                    <Button>Login</Button>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
